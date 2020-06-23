71c45a91fe354b16071ac72507f917bf7cae46bf
馬込浩 <yamorijakarta@magomekou-no-MacBook-Air.local>
2020-06-23 22:03:57 +0900

    add root url vies

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("kanban.urls")),
     path("kanban/", include("kanban.urls")),
     path('kanban/', include('django.contrib.auth.urls')),  # この行を追加
     path('admin/', admin.site.urls),
